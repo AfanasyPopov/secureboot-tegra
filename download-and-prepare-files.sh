c4f1b96ab4892d3c07299fd7f1bb29f98c2621eb
AfanasyPopov <afanasy.popov@gmail.om>
2024-09-10 01:36:24 -0700

    chenge kernel to 32.6.1

--- download-and-prepare-files.sh
+++ download-and-prepare-files.sh
@@ -5,13 +5,13 @@ source ./downloadfilenames
 mkdir -p ${DOWNLOADS_DIR}
 pushd ${DOWNLOADS_DIR}
 if [ ! -f ${L4T_RELEASE_PACKAGE} ]; then
-    wget https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/${L4T_RELEASE_PACKAGE_NAME}
+    wget https://developer.nvidia.com/embedded/${URL_DIR_L4T_RELEASE_PACKAGE_NAME}/${L4T_RELEASE_PACKAGE_NAME}
 fi
 if [ ! -f ${SECUREBOOT_PACKAGE} ]; then
-    wget https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/${SECUREBOOT_PACKAGE_NAME}
+    wget https://developer.nvidia.com/embedded/${URL_DIR_L4T_RELEASE_PACKAGE_NAME}/${SECUREBOOT_PACKAGE_NAME}
 fi
 if [ ! -f ${SAMPLE_FS_PACKAGE} ]; then
-    wget https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/${SAMPLE_FS_PACKAGE_NAME}
+    wget https://developer.nvidia.com/embedded/${URL_DIR_L4T_RELEASE_PACKAGE_NAME}/${SAMPLE_FS_PACKAGE_NAME}
 fi
 popd
 ./preparing-for-use.sh
